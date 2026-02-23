e1ce4794073c2f1d0973445bc36726f76dcddc78
laura <laura.oliveira.rodrigues@escola.pr.gov.br>
2026-02-23 12:36:11 +0000

    Criar variáveis para Pessoa e Produto

---
+++ variavel-produto.js
@@ -0,0 +1,13 @@
+/**
+ * CRIE 5 VARIÁVEIS PARA CARACTERÍSTICAS DE UM PRODUTO DO MERCADO LIVRE
+ * CONSIDERE SE A VARIÁVEL ,É let ou const 
+ * IMPRIMA COM console.log (<variável>); 
+ * IMPRIMA COM console.log ("NOME-VARIAVEL: " + <variavel>)
+ */
+let NomeProduto = "Camisa"
+let Tamanho = "Único"
+let Cor = "Preto"
+
+
+
+
