90afec24ae9b4a4ed0e60eb35e66cf9cc7e24d4d
laura <laura.oliveira.rodrigues@escola.pr.gov.br>
2026-03-09 11:34:33 +0000

    métodos array: inserçaõ, deleção e impressão

--- arrays/array-time.js
+++ arrays/array-time.js
@@ -9,7 +9,10 @@ time [5] // undefined
 time [6] = "Victor" // atribuição dinâmicos
 
 for (const jogador of time) {
-    console.log(jogador)
+    if (jogador != undefined) {
+        console.log(jogador)
+    }
+
 }
 console.log (time)
 console.log (time[1])
