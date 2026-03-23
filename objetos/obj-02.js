93731acc4bb5b477ceafeeb7143abc9d1af41ec9
laura <laura.oliveira.rodrigues@escola.pr.gov.br>
2026-03-23 12:20:33 +0000

    Finalização de Objetos JS

---
+++ objetos/obj-02.js
@@ -0,0 +1,24 @@
+// Crie um objeto do tema que escolher
+// Crie pelo menos 5 propriedades
+// Faça uma impressão para Humanos!
+// ex. console.log("Nome do Jogador: " + jogador.nome)
+
+const mercado = {
+    Nome: "Atacadão",
+    Local: "Boa Vista",
+    Vagas: ["Açougue", "Padaria", "Limpeza"],
+    HoráriodeAtendimento: "Segunda a sábado, das 07h às 22h, e aos domingos, das 07h às 20h",
+    DatadeFundação: "1962"
+}
+mercado.nome = "Atacadão"
+mercado.Local = "Boa Vista"
+mercado.Vagas = "Açougue", "Padaria", "Limpeza"
+mercado.HoráriodeAtendimento = "Segunda a sábado, das 07h às 22h, e aos domingos, das 07h às 20h"
+mercado.DatadeFundação = "1962"
+
+console.log(mercado.nome)
+console.log(mercado.Local)
+console.log(mercado.Vagas)
+console.log(mercado.HoráriodeAtendimento)
+console.log(mercado.DatadeFundação)
+console.log("Atacadão:" + mercado.Local)
