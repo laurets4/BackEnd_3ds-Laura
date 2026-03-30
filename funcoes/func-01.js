d899d2f529f2ac0bc95879478d063aaa9411cbcd
laura <laura.oliveira.rodrigues@escola.pr.gov.br>
2026-03-30 11:02:07 +0000

    Finalizamos as funções

---
+++ funcoes/func-01.js
@@ -0,0 +1,16 @@
+// Funções são blocos de código reaproveitáveis
+// podem ou Não receber parâmetros 
+//podem ou não retornar valores
+
+// declaração
+function dizOla(){
+    console.log("Ola")
+}
+function olaPessoa(nome){
+    console.log("Olá, " + nome)
+}
+
+//inovação
+dizOla()
+olaPessoa("Laura")
+olaPessoa("Parça")
---
+++ funcoes/func-02.js
@@ -0,0 +1,32 @@
+// Crie 4 funções para operações (+, -, *, /)
+// Calcula e imprime resultado
+
+function subtracao(n1, n2){
+    const resultado = n1 - n2
+    console.log("Resultado da subtração: " + resultado)
+}
+
+//inovação
+subtracao(7,3)
+subtracao(19, 14)
+
+function  adicao(n1, n2){
+    const resultado = n1 + n2
+    console.log("Resultado da adição: " + resultado)
+}
+adicao(19, 45)
+adicao(23, 12)
+
+function divisao(n1, n2){
+    const resultado = n1 / n2
+    console.log("Resultado da divisao: " + resultado)
+}
+divisao(54, 7)
+divisao(76, 23)
+
+function multiplicacao(n1, n2){
+    const resultado = n1 * n2
+    console.log("Resultado da multiplicação: " + resultado)
+}
+multiplicacao(56, 4)
+multiplicacao(35, 67)
