870ece7e96a806b2e418ed1b8a07da3b8515b782
laura <laura.oliveira.rodrigues@escola.pr.gov.br>
2026-05-27 11:49:40 +0000

    Finalizaçaõ do Módulo FS com Path (join e resolve)

--- 2° trismetre/modulos/mod03-fs/fs-com-path.js
+++ 2° trismetre/modulos/mod03-fs/fs-com-path.js
@@ -1,9 +1,12 @@
 // importar PATHE E FS 
-
-
+// Path possui (join para jutar caminhos e Resolve
+// para deduzir subdiretórios e S.O.)
+const fs = require('fs')
+const path = require('path')
 
 // Salvar o Caminho em um variável 
+//const caminho = path.join(__dirname, 'README.md')
+const caminho = path.resolve(__dirname, 'README.md') // Abrir, ler, deletar, editar
+const arquivo = fs.readFileSync(caminho, 'utf-8')
 
-
-
-// Abrir, ler, deletar, editar
+console.log(arquivo)
