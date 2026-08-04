b6b5c7428486dfde129ebc6d9a88b68ea746af49
laura <laura.oliveira.rodrigues@escola.pr.gov.br>
2026-08-04 11:52:40 +0000

    Adiciona páginas HTML e implementa rotas no servidor

---
+++ 2° trismetre/modulos/mod03-server/exercicio-htlm.md
@@ -0,0 +1,6 @@
+### Exercício HTML e ROTAS 
+
+  - A partir da estrutura desta pasta "SERVER03":
+  - Crie HTML com TEMA LIVRE PARA AS PÁGINAS:
+  - index.html, sobre.html, 404.html (página não encontrada)
+  - 
--- 2° trismetre/modulos/mod03-server/server01.js
+++ 2° trismetre/modulos/mod03-server/server01.js
@@ -1,22 +1,38 @@
 const http = require('node:http')
-const porta = 8002
+const path = require('node:path')
+const fs = require('node:fs')
+const porta = 8081
+
+
+const home = path.join(__dirname, 'pages/index.html')
+const sobre = path.join(__dirname, 'pages/sobre.html')
+
 
 const server = http.createServer((req, res)=>{
-    const novaURL = new URL(req.url, `http://${req.headers.host}`) //http://localhost
-    const caminhoURL = novaURL.pathname
-    if(caminhoURL === '/'){
-        res.statusCode = '201'
+    const novaUrl = new URL(req.url, `http://${req.headers.host}`) //http://localhost
+    const caminhoUrl = novaUrl.pathname
+
+    if(caminhoUrl === '/'){
+
+        res.statusCode = '200'
         res.setHeader('Content-Type', 'text/html; charset=utf-8')
-        res.end('<h3>HTML Rocks!</h3>')
-    }else {
+        return res.end(fs.readFileSync(home, 'utf-8'))
+    } 
     
+    if(caminhoUrl === '/sobre'){
+
+        res.statusCode = '200'
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        return res.end(fs.readFileSync(sobre, 'utf-8'))
+    } 
+    else { 
+
     res.statusCode = '401'
     res.setHeader('Content-Type', 'text/html; charset=utf-8')
-    res.end('<h3>401 Não autorizado!</h3>')
-    }
+    res.end('<h3>401 Não Autorizado</h3>')
+}
 })
-
 server.listen(porta, ()=>{
     console.log(`Servidor rodando na porta 
         http://local""host:${porta}`)
-})
+    })
