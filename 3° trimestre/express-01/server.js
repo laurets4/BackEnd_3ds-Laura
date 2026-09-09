85b23451f3edb5b17cb8f6cc219d9a6862af6682
laura <laura.oliveira.rodrigues@escola.pr.gov.br>
2026-09-09 11:50:00 +0000

    Rotas básicas html inline

---
+++ 3° trimestre/express-01/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const PORT = 3000
+const app = express()
+
+app.get('/', (req, res) => { // calback ou retorno
+    res.send('<h3>Hello Pet!</h3>')
+})  
+app.get('/servicos', (req, res) => { // calback ou retorno 
+    res.send('<h3>Serviços Pet!</h3>')
+}) 
+app.get('/produtos', (req, res) => { // calback ou retorno 
+    res.send('<h3>Produtos Pet!</h3>')
+}) 
+app.listen(PORT, () => {console.log('Servidor Vivo!')})
